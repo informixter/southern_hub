3032c80b9be7d3d8ec3affd5cead235e04e5783b
informixter <pbd.incom@gmail.com>
2020-08-22 16:46:16 +0500

    Лейдблы

---
+++ api/database/factories/LabelsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Labels;
+use Faker\Generator as Faker;
+
+$factory->define(Labels::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
