91742c077b14341ca1b7b754737743dcab36f9e6
informixter <pbd.incom@gmail.com>
2020-08-22 17:01:52 +0500

    Сидеры

--- api/database/seeds/DatabaseSeeder.php
+++ api/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        DB::table('models')->insert([
+            'name' => "Первая модель",
+            'status' => 'markup',
+            'type' => 'classification',
+        ]);
+
+        DB::table('models')->insert([
+            'name' => "Вторая модель",
+            'status' => 'markup',
+            'type' => 'classification',
+        ]);
+
+        DB::table('labels')->insert([
+            'name' => "first_model_label_1",
+            'model_id' => 1,
+        ]);
+
+        DB::table('labels')->insert([
+            'name' => "first_model_label_2",
+            'model_id' => 1,
+        ]);
+
+        $texts = [
+            'Какие категории вопросов и задач можно решать в рамках технической поддержки?',
+            'Как обратиться в службу технической поддержки, если не получается войти в консоль управления?',
+            'Как быстро служба технической поддержки отреагирует на обращение?',
+            'Как быстро служба технической поддержки решит выявленную проблему?',
+            'Сколько раз можно обращаться в техническую поддержку?',
+            'Как получить помощь в решении архитектурных задач?',
+            'На какое программное обеспечение сторонних производителей распространяется техническая поддержка Яндекс.Облака?',
+        ];
+
+        foreach ($texts as $text) {
+            DB::table('texts')->insert([
+                'text' => $text,
+                'model_id' => 1,
+            ]);
+        }
+
+
     }
 }
--- api/database/seeds/ModelsSeeder.php
+++ api/database/seeds/ModelsSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class ModelsSeeder extends Seeder
 {
@@ -11,6 +13,16 @@ class ModelsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('models')->insert([
+            'name' => "Первая модель",
+            'status' => 'markup',
+            'type' => 'classification',
+        ]);
+
+        DB::table('models')->insert([
+            'name' => "Вторая модель",
+            'status' => 'markup',
+            'type' => 'classification',
+        ]);
     }
 }
