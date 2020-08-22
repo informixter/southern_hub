c804b0544a205ab0cf98461addeb9b3aa6f53ecd
informixter <pbd.incom@gmail.com>
2020-08-22 17:42:28 +0500

    fast fix

--- api/database/migrations/2020_08_22_121437_labels_texts_update.php
+++ api/database/migrations/2020_08_22_121437_labels_texts_update.php
@@ -13,7 +13,7 @@ class LabelsTextsUpdate extends Migration
      */
     public function up()
     {
-        Schema::create('labels', function (Blueprint $table) {
+        Schema::create('labels_texts', function (Blueprint $table) {
             $table->integer("model_id")->unsigned()->index();
             $table->integer("text_id")->unsigned()->index();
         });
@@ -26,6 +26,6 @@ class LabelsTextsUpdate extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('labels');
+        Schema::dropIfExists('labels_texts');
     }
 }
